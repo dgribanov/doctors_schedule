ca381276fa07e3ed78a39b23172c9d610c14f3a1
dgribanov <denisgribanov@yandex.ru>
2015-01-02 00:15:40 +0200

    db component configuration

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=doctors_schedule',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
